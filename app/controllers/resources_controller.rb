98574d8ede3a40a447f85a25f977da4204b5f6dd
Michele Gozzi <michelegozzi@gmail.com>
2013-09-15 21:22:48 +0200

    debug

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,7 +4,12 @@ class ResourcesController < ApplicationController
 
     if !@resource.nil? && !@resource.mime_type.nil? && !@resource.encoded_file.nil?
       decoded = Base64.decode64(@resource.encoded_file)
-      send_data decoded, :type => @resource.mime_type, :disposition => 'inline' 
+      send_data decoded, :type => @resource.mime_type, :disposition => 'inline'
+    else
+      msg = 'Causes: ' + @resource.nil? ? 'nil resource' : ''
+      msg += ', nil mime type' if @resource.mime_type.nil?
+      msg += ', nil encoded file' if @resource.encoded_file.nil?
+      flash[:error] = "Profile updated"
     end
   end
 end
