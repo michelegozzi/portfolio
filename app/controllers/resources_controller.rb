9b3f951c22fb7de53ef57775203e0fb28844b86c
Michele Gozzi <michelegozzi@gmail.com>
2013-09-15 21:11:17 +0200

    problem with heroku

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,10 @@
 class ResourcesController < ApplicationController
   def show
     @resource = Resource.find(params[:id])
-    send_data Base64.decode64(@resource.encoded_file), :type => @resource.mime_type, :disposition => 'inline' 
+
+    if !@resource.nil? && !@resource.mime_type.nil? && !@resource.encoded_file.nil?
+      decoded = Base64.decode64(@resource.encoded_file)
+      send_data decoded, :type => @resource.mime_type, :disposition => 'inline' 
+    end
   end
 end
